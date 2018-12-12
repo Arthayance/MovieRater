92df5c9e630ea139ea4ce3abfe597993e729e7ad
Jia Rong <arthayance@hotmail.com>
2018-12-12 13:42:10 +0800

    1st commit

---
+++ app/src/main/java/com/nyp/sit/dit/it2107/movierater/Main4Activity.kt
@@ -0,0 +1,43 @@
+package com.nyp.sit.dit.it2107.movierater
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main4.*
+import org.w3c.dom.Text
+
+class Main4Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+
+        registerForContextMenu(pressMe)
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        if (v?.id == R.id.pressMe) {
+            menu?.add(1, 1001, 1, "Add")
+        }
+    }
+
+    override fun onContextItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == 1001) {
+            finish()
+        }
+        return super.onContextItemSelected(item)
+    }
+
+    fun addMovie (v: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+
+    }
+
+}

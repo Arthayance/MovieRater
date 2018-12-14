9bba1a11360254fcb6c078473a220119ba65d1f3
Jia Rong <arthayance@hotmail.com>
2018-12-14 19:35:51 +0800

    1st commit

---
+++ app/src/main/java/com/nyp/sit/dit/it2107/movierater/MovieEntity.kt
@@ -0,0 +1,16 @@
+package com.nyp.sit.dit.it2107.movierater
+
+import android.widget.CheckBox
+import android.widget.CheckedTextView
+import android.widget.RadioButton
+
+public class MovieEntity {
+    var movietitle: String = ""
+    var movie_desc: String = ""
+    var movie_lang: String = ""
+    var movie_rdate: String = ""
+    var movie_audience: Boolean = true
+    var reasonViolence: Boolean = true
+    var reasonLanguage: Boolean = true
+    public var Rating: Float = 0.00F
+}
